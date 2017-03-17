4ddb5d012d663204abbe26155d73b1ad5da2b93f
anusha <singhanusha744@gmail.com>
2017-03-17 12:26:26 +0545

    this is the fourth commit

---
+++ basictypes.py
@@ -0,0 +1,7 @@
+an_integer= 55
+a_floating_point =11.33
+a_boolean = True
+a_string = "Foo"
+print "Integer value =",an_integer
+print "Float value =" ,a_floating_point
+print "Boolean value =" ,a_string
