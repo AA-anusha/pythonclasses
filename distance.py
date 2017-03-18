ef0a0ca7e33dbb671caacd81c91116f5dde15d02
anusha <singhanusha744@gmail.com>
2017-03-18 08:54:36 +0545

    this is the commit for strings

---
+++ distance.py
@@ -0,0 +1,6 @@
+x1 = 5.0
+x2 = 10.0
+y1 = 6.0
+y2 = 20.0
+distance = ((x2- x1) ** 2 + (y2- y1) ** 2) ** (0.5)
+print "distance between two points",distance
---
+++ quadratic.py
@@ -0,0 +1,10 @@
+a=raw_input("enter a")
+
+b=raw_input("enter b")
+c=raw_input("enter c")
+
+d = (b ** 2 - 4 * a * c) ** (0.5)
+x1=(-b +  d) / (2 * a)
+x2=(-b -  d) / (2 * a)
+print "quadratic value is",x1
+print "another quadratic value is",x2
