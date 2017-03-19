e7bca73d1bfa319c8b279574d95f1f094bf5a936
anusha <singhanusha744@gmail.com>
2017-03-19 13:19:28 +0545

    this is the commit for capitalized

---
+++ testofnames.py
@@ -0,0 +1,3 @@
+names =["Anusha","Nirose","ayush","Nilesh","Rame","anushe","hare","Aalu","John","doe","hero" ]
+names_with_e=[x for x in names if 'e' in "names"]
+print("names with e are:",names_with_e)
